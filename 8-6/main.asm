c2fc03d9c2be6df815869cffed25a0786c2a43d7
Temomik <31627314+Temomik@users.noreply.github.com>
2019-04-17 13:38:54 +0300

    lab 8 update

--- 8-6/main.asm
+++ 8-6/main.asm
@@ -4,46 +4,53 @@
 org 100h
 start:
       jmp begin
-
-get_cmos_value macro source,receiver
+ 
+     
+;source - number of register, reciver - register for result value 
+get_part_time macro source,receiver
       mov al, source
-      out 70h, al
-      in al, 71h    
+      out 70h, al       ;print register into port
+      in al, 71h        ;get info from register
       mov receiver, al
 endm
+ 
+ 
+ 
+;value - bcd number, source - place for result  
+bcd_to_int macro value, source
+      pusha                     
+      
+      mov al,value
+      and al,00001111b    ;younger 4 bits
+      add al, '0'         ;from string to number
+      mov cs:source[2], al     ;write first digit into time string
 
-bcd_to_decimal macro bcd_num, source
-      pusha
-      mov al,bcd_num
-      and al,00001111b
-      add al, '0'
-      mov cs:source[2], al 
-
-      mov al,bcd_num
-      shr al, 4
-      add al, '0'
-      mov cs:source[0], al      
+      mov al,value
+      shr al, 4           ;>>4, now we have older 4 bits
+      add al, '0'         ;string into number
+      mov cs:source[0], al    ;write secind digit
+        
       popa     
 endm
-matrix  db 15,4000 dup(15)
 
 time_str db '0',2Eh,'0',2Eh,':',2Eh,'0',2Eh,'0',2Eh,':',2Eh,'0',2Eh,'0', 2Eh
 old_handler dd ?
-position dw 0
-x_position dw ?
-y_position dw ?
-msg_err db 'Wrong parametrs,', 0dh, 0ah, 'input 0 <= x < 72 and 0 <= y < 24', 0dh, 0ah, '$'
+coordinate dw 0
+pos_x dw ?
+pos_y dw ?
+error_message db 'Invalid command line parameters!', 0dh, 0ah, '$'
 
 get_time proc
-      pusha
-      get_cmos_value 0,dl ; sec
-      bcd_to_decimal dl,time_str[12]
+      pusha  
       
-      get_cmos_value 2,dl ; min
-      bcd_to_decimal dl,time_str[6]
+      get_part_time 0,dl ; seconds
+      bcd_to_int dl,time_str[12]
       
-      get_cmos_value 4,dl ; hour
-      bcd_to_decimal dl,time_str[0]
+      get_part_time 2,dl ; minutes
+      bcd_to_int dl,time_str[6]
+      
+      get_part_time 4,dl ; hours
+      bcd_to_int dl,time_str[0]
 
       popa
       ret
@@ -55,17 +62,17 @@ output_time proc
       push ds
       push es
 
-      mov ax, 0B800h     
+      mov ax, 0B800h      ;segment of display
       mov es, ax         
 
       mov ax, cs
-      mov ds, ax
-      mov di, position
+      mov ds, ax          ;code segment into data (cause constants in .code)
+      mov di, coordinate
       lea si, time_str
       mov cx, 16
 
-      cld
-      rep movsb
+      cld                  ;df=0
+      rep movsb            ;si->di
 
       pop es
       pop ds
@@ -74,123 +81,155 @@ output_time proc
       ret
 endp
 
+
 skip_space macro str 
-      local skip_space
+      local skip_space            ;local variable  
+      
       skip_space:
       inc si
       cmp byte ptr es:[si]," "
       je skip_space
 endm
 
+ 
 skip_letters macro str 
-      local skip_letters,skip_letters_end
+      local skip_letters,skip_letters_end 
+      
       skip_letters:
       inc si
+      
       cmp byte ptr es:[si]," "
-            je skip_letters_end
-      cmp byte ptr es:[si],10
-            je skip_letters_end
-      cmp byte ptr es:[si],13
-            je skip_letters_end
-      jmp skip_letters
+            je skip_letters_end       
+            
+      cmp byte ptr es:[si],10           ;new line
+            je skip_letters_end       
+            
+      cmp byte ptr es:[si],13           ;begin of line
+            je skip_letters_end    
+            
+      jmp skip_letters 
+      
       skip_letters_end:
-endm
-
+endm 
+ 
+ 
 convert_to_num macro str, num 
       local convert_to_num_cycle,convert_to_num_cycle_end
-      pusha
+      pusha                                           
+      
       xor bx,bx
-      xor ax,ax
+      xor ax,ax      
+      
       convert_to_num_cycle:
       cmp byte ptr str[bx],'9'
-            jg error
-      cmp byte ptr str[bx],'3'
-            ; jl error
+            jg error                 
       cmp byte ptr str[bx]," "
             je convert_to_num_cycle_end
       cmp byte ptr str[bx],13
             je convert_to_num_cycle_end
       cmp byte ptr str[bx],10
-            je convert_to_num_cycle_end
+            je convert_to_num_cycle_end    
+            
       mov dx,10
       imul dx        
-            jo error
+      jo error                 
+      
       xor dx,dx
       mov dl, si[bx]
-      sub dl,'0'
+      sub dl,'0'                ;convert into num 
+      
       add ax, dx       
-            jo error
+      jo error                  
+      
       mov num,ax
       inc bx
-      jmp convert_to_num_cycle
+      jmp convert_to_num_cycle  
+      
       convert_to_num_cycle_end:
       popa
-endm
-
-get_position proc near
-      mov si,81h
-      skip_space si
-      convert_to_num si,x_position
-      cmp x_position,72
-            jg error
+endm 
+ 
+ 
+ 
+ 
+get_coordinates_from_cmd proc near
+      mov si,81h                    ;cmd text
+      
+      skip_space si                 
+      
+      convert_to_num si,pos_x       ;input first number with str->num
+      
+      cmp pos_x,72
+      jg error               
+      
       skip_letters si
       ; add si,1
-      skip_space si
-      convert_to_num si,y_position
-      cmp y_position,24
-            jg error
+      skip_space si         
+      
+      convert_to_num si,pos_y
+      cmp pos_y,24
+      jg error
       
       
       xor ax,ax
-      mov ax,y_position
+      mov ax,pos_y
       mov dx,160
       imul dx        
-            jo error
-      add ax, x_position
-      add ax, x_position
-      sub ax,2
-      mov position,ax
+      jo error
+      add ax, pos_x
+      add ax, pos_x
+      ;sub ax,2
+      
+      mov coordinate,ax
       ret
 endp
 
 
-int_handler proc far
-      cli
-      call get_time
-      call output_time
-      jmp cs:old_handler
-      sti
+
+
+int_handler proc far    ;interrupt handler
+   cli
+   
+   call get_time
+   call output_time
+   jmp cs:old_handler
+    
+   sti 
    iret
 endp
 
 
-begin:      
-      call get_position
-
-      mov ax, 351Ch
-      int 21h
-      mov word ptr old_handler, bx
+begin:
+      call get_coordinates_from_cmd  
+       
+      mov ax, 351Ch     ;get old handler (result in bx)
+      int 21h            
+      
+      mov word ptr old_handler, bx     ;save old handler
       mov word ptr old_handler + 2, es
 
-      mov ax, 251Ch
+      mov ax, 251Ch                    ;tell dos to set new handler from dx
       lea dx, int_handler
       int 21h
 
-      jmp set_res
+      jmp finish
 
 error:
 
       mov ah, 9
-      lea dx, msg_err
+      lea dx, error_message      ;out error
       int 21h
-      jmp end_
+      jmp end_main
 
-set_res:
-      mov ax, 3100h
-      mov dx, (begin - start + 10Fh) / 16
-      int 21h  
-      ret
-end_:
-      mov ah,4ch
-      int 21h
+
+finish:
+       mov ax, 3100h   ;keep resident
+       mov dx, (begin - start + 10Fh) / 16   ;size of resident part
+       int 21h
+       ret
+       
+end_main:
+       mov ah, 4ch
+       int 21h
+
 end start
